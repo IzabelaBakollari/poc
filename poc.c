81b9cc66a182f8bba758a244970064a1f793e2b3
Izabela bakollari <izabela.bakollari@gmail.com>
2021-10-11 09:44:38 +0200

    Passing argument by reference

--- poc.c
+++ poc.c
@@ -10,53 +10,53 @@
 #include <linux/can.h>
 #include <linux/can/bcm.h>
 
-void
-rxsetup_sock(int sock)
-{
-	struct {
-		struct bcm_msg_head b;
-		struct canfd_frame f;
-	} msg;
+struct message{
+	struct bcm_msg_head b;
+	struct canfd_frame f;
+};
 
+void
+rxsetup_sock(struct message *msg)
+{
 	memset(&msg, 0, sizeof(msg));
 
-        msg.b.opcode = RX_SETUP;
-        msg.b.flags = CAN_FD_FRAME | SETTIMER | STARTTIMER;
-        msg.b.count = 0;
-        msg.b.ival1.tv_sec = msg.b.ival2.tv_sec = 0;
-        msg.b.ival1.tv_usec = msg.b.ival2.tv_usec = 1;
-        msg.b.can_id = 0;
-        msg.b.nframes = 1;
+        msg->b.opcode = RX_SETUP;
+        msg->b.flags = CAN_FD_FRAME | SETTIMER | STARTTIMER;
+        msg->b.count = 0;
+        msg->b.ival1.tv_sec = msg->b.ival2.tv_sec = 0;
+        msg->b.ival1.tv_usec = msg->b.ival2.tv_usec = 1;
+        msg->b.can_id = 0;
+        msg->b.nframes = 1;
 }
 
 void
-txsetup_sock(int sock)
+txsetup_sock(struct message *msg)
 {
-	struct {
-		struct bcm_msg_head b;
-		struct canfd_frame f;
-	} msg;
-
 	memset(&msg, 0, sizeof(msg));
 
-	msg.b.opcode = TX_SETUP;
-        msg.b.flags = CAN_FD_FRAME | SETTIMER | STARTTIMER | TX_COUNTEVT;
-        msg.b.count = 2;
-        msg.b.ival1.tv_sec = msg.b.ival2.tv_sec = 1;
-        msg.b.ival1.tv_usec = msg.b.ival2.tv_usec = 1;
-        msg.b.can_id = 0;
-        msg.b.nframes = 1;
+	msg->b.opcode = TX_SETUP;
+        msg->b.flags = CAN_FD_FRAME | SETTIMER | STARTTIMER | TX_COUNTEVT;
+        msg->b.count = 2;
+        msg->b.ival1.tv_sec = msg->b.ival2.tv_sec = 1;
+        msg->b.ival1.tv_usec = msg->b.ival2.tv_usec = 1;
+        msg->b.can_id = 0;
+        msg->b.nframes = 1;
 }
 
 void
-receive_and_check(int sock)
+receive_and_check(struct message *msg)
 {
 	int i,s;
+	int sock;
 	struct sockaddr_can sa;
-	struct {
-		struct bcm_msg_head b;
-		struct canfd_frame f;
-	} msg;
+
+	sock = socket(AF_CAN, SOCK_DGRAM, CAN_BCM);
+
+	if (sock < 0) {
+		perror("sock");
+		printf("Errno = %d\n", errno);
+		exit(EXIT_FAILURE);
+	}
 
 	sa.can_family = AF_CAN;
 	sa.can_ifindex = 6;
@@ -81,7 +81,7 @@ receive_and_check(int sock)
 	s = recvfrom(sock, &msg, sizeof(msg), 0,
 			(struct sockaddr *)&sa, &len);
 
-	if (s < sizeof(msg.b)) {
+	if (s < sizeof(msg->b)) {
        		perror("Message recevied is null");
 		printf("Errno = %d\n", errno);
 		exit(EXIT_FAILURE);
@@ -116,8 +116,7 @@ main(void)
 	if (sock < 0) {
 		perror("sock");
 		printf("Errno = %d\n", errno);
-
-        return 0;
+		exit(EXIT_FAILURE);
 	}
 
 	sa.can_family = AF_CAN;
@@ -127,7 +126,7 @@ main(void)
 
 	connect(sock, (struct sockaddr *)&sa, sizeof(sa));
 
-	rxsetup_sock(sock);
+	rxsetup_sock(&msg);
 
 	memset(&sa, 0, sizeof(sa));
 	sa.can_family = AF_CAN;
@@ -135,11 +134,11 @@ main(void)
 
 	memset(&msg, 0, sizeof(msg));
 	
-	receive_and_check(sock);
+	receive_and_check(&msg);
 
-	txsetup_sock(sock);
+	txsetup_sock(&msg);
 
-	receive_and_check(sock);
+	receive_and_check(&msg);
 
 	return 0;
 }
