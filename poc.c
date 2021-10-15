4368bc6ab57eee02ddeac3ba56e17e1dff95d0d6
Michal Schmidt <mschmidt@redhat.com>
2021-10-15 15:58:09 +0200

    more improvements

--- poc.c
+++ poc.c
@@ -3,6 +3,7 @@
 #include <net/if.h>
 #include <sys/socket.h>
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,18 +14,26 @@ struct message {
 	struct canfd_frame f;
 };
 
-void print_message(struct message *msg, int len)
-{	
+static void print_message(struct message *msg, int len)
+{
 	int i;
 
-	for (i = 0; i < len; i++)
-    		printf("%x ", ((unsigned char*) msg)[i]);
-   	printf("\n");
+	for (i = 0; i < len; i++) {
+		if (i % 16 == 0)
+			printf("%04x  ", i);
+		printf("%02x ", ((unsigned char*) msg)[i]);
+		if (i % 16 == 15)
+			printf("\n");
+	}
+
+	if (i % 16)
+		printf("\n");
 }
 
-void communicate_and_check(struct message *msg, int sock, struct sockaddr_can *sa, __u32 expected_opcode)
+static void communicate_and_check(struct message *msg, int sock, const struct sockaddr_can *sa, __u32 expected_opcode)
 {
-	int i, r, n;
+	int i, r;
+	char n;
 
 	printf("Sending:\n");
 	print_message(msg, sizeof(*msg));
@@ -32,24 +41,31 @@ void communicate_and_check(struct message *msg, int sock, struct sockaddr_can *s
 	r = sendto(sock, msg, sizeof(*msg), 0, (struct sockaddr *)sa, sizeof(*sa));
 	if (r < 0) {
 		perror("sendto");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	r = recvfrom(sock, msg, sizeof(*msg), 0, NULL, NULL);
+	if (r < 0) {
+		perror("recvfrom");
+		exit(EXIT_FAILURE);
+	}
+
 	printf("Received:\n");
 	print_message(msg, r);
 	if (r < sizeof(msg->b)) {
-       		perror("Did not receive a full message");
+		fprintf(stderr, "The received message is too short, only %d bytes.\n", r);
 		exit(EXIT_FAILURE);
 	}
 
 	if (msg->b.opcode != expected_opcode) {
-		fprintf(stderr, "Unexpected reply opcode received: %u\n", msg->b.opcode);
+		fprintf(stderr, "Unexpected opcode in the reply: 0x%08x\n", msg->b.opcode);
 		exit(EXIT_FAILURE);
 	}
-    
-	for (i = 12; i < 16; i++) {
-		n = ((unsigned char*) msg)[i];
+
+	for (i = offsetof(struct bcm_msg_head, count) + sizeof(((struct bcm_msg_head){}).count);
+	     i < offsetof(struct bcm_msg_head, ival1);
+	     i++) {
+		n = ((char*) msg)[i];
 		if (n != 0) {
 			fprintf(stderr, "Non-zero padding byte in the reply!\n");
 			exit(EXIT_FAILURE);
@@ -60,7 +76,6 @@ void communicate_and_check(struct message *msg, int sock, struct sockaddr_can *s
 int main(int argc, char *argv[])
 {
 	int sock, r;
-	struct sockaddr_can sa;
 	struct message msg;
 	unsigned ifindex;
 
@@ -75,19 +90,18 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	memset(&sa, 0, sizeof(sa));
-	sa.can_family = AF_CAN;
-	sa.can_ifindex = ifindex;
-	sa.can_addr.tp.rx_id = 0;
-	sa.can_addr.tp.tx_id = 0;
+	const struct sockaddr_can sa = {
+		.can_family = AF_CAN,
+		.can_ifindex = ifindex,
+	};
 
 	sock = socket(AF_CAN, SOCK_DGRAM, CAN_BCM);
 	if (sock < 0) {
-		perror("sock");
+		perror("socket");
 		exit(EXIT_FAILURE);
 	}
 
-	r = connect(sock, (struct sockaddr *)&sa, sizeof(sa));
+	r = connect(sock, (const struct sockaddr *)&sa, sizeof(sa));
 	if (r < 0) {
 		perror("connect");
 		exit(EXIT_FAILURE);
@@ -98,7 +112,6 @@ int main(int argc, char *argv[])
 		.b.flags = CAN_FD_FRAME | SETTIMER | STARTTIMER | TX_COUNTEVT,
 		.b.count = 2,
 		.b.ival1 = { 0, 1 },
-		.b.ival2 = { 0, 1 },
 		.b.nframes = 1,
 	};
 	communicate_and_check(&msg, sock, &sa, TX_EXPIRED);
@@ -107,7 +120,6 @@ int main(int argc, char *argv[])
 		.b.opcode = RX_SETUP,
 		.b.flags = CAN_FD_FRAME | SETTIMER | STARTTIMER,
 		.b.ival1 = { 0, 1 },
-		.b.ival2 = { 0, 1 },
 		.b.nframes = 1,
 	};
 	communicate_and_check(&msg, sock, &sa, RX_TIMEOUT);
@@ -116,10 +128,11 @@ int main(int argc, char *argv[])
 		.b.opcode = TX_SEND,
 		.b.flags = CAN_FD_FRAME,
 		.b.nframes = 1,
+
 		.f.len = 1,
 		.f.data = { 0x42, },
 	};
 	communicate_and_check(&msg, sock, &sa, RX_CHANGED);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
